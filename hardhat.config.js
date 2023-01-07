4111d706960dc7d9eec27ae9de17c9047f764abe
bpssm <bernastroll@gmail.com>
2023-01-07 14:13:23 +0000

    Shimmer and Milkomeda Implementation

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,25 +4,12 @@ require("hardhat-contract-sizer")
 require('@typechain/hardhat')
 
 const {
-  BSC_URL,
-  BSC_DEPLOY_KEY,
-  BSCSCAN_API_KEY,
-  POLYGONSCAN_API_KEY,
-  SNOWTRACE_API_KEY,
-  ARBISCAN_API_KEY,
-  ETHERSCAN_API_KEY,
-  BSC_TESTNET_URL,
-  BSC_TESTNET_DEPLOY_KEY,
-  ARBITRUM_TESTNET_DEPLOY_KEY,
-  ARBITRUM_TESTNET_URL,
-  ARBITRUM_DEPLOY_KEY,
-  ARBITRUM_URL,
-  AVAX_DEPLOY_KEY,
-  AVAX_URL,
-  POLYGON_DEPLOY_KEY,
-  POLYGON_URL,
-  MAINNET_URL,
-  MAINNET_DEPLOY_KEY
+  SHIMMER_URL,
+  SHIMMER_DEPLOY_KEY,
+  SHIMMER_API_KEY,
+  MILKOMEDA_URL,
+  MILKOMEDA_DEPLOY_KEY,
+  MILKOMEDA_API_KEY
 } = require("./env.json")
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -49,55 +36,23 @@ module.exports = {
     hardhat: {
       allowUnlimitedContractSize: true
     },
-    bsc: {
-      url: BSC_URL,
-      chainId: 56,
+    shimmer: {
+      url: SHIMMER_URL,
+      chainId: 1074,
       gasPrice: 10000000000,
-      accounts: [BSC_DEPLOY_KEY]
+      accounts: [SHIMMER_DEPLOY_KEY]
     },
-    testnet: {
-      url: BSC_TESTNET_URL,
-      chainId: 97,
+    milkomeda: {
+      url: MILKOMEDA_URL,
+      chainId: 2100,
       gasPrice: 20000000000,
-      accounts: [BSC_TESTNET_DEPLOY_KEY]
-    },
-    arbitrumTestnet: {
-      url: ARBITRUM_TESTNET_URL,
-      gasPrice: 10000000000,
-      chainId: 421611,
-      accounts: [ARBITRUM_TESTNET_DEPLOY_KEY]
-    },
-    arbitrum: {
-      url: ARBITRUM_URL,
-      gasPrice: 30000000000,
-      chainId: 42161,
-      accounts: [ARBITRUM_DEPLOY_KEY]
-    },
-    avax: {
-      url: AVAX_URL,
-      gasPrice: 200000000000,
-      chainId: 43114,
-      accounts: [AVAX_DEPLOY_KEY]
-    },
-    polygon: {
-      url: POLYGON_URL,
-      gasPrice: 100000000000,
-      chainId: 137,
-      accounts: [POLYGON_DEPLOY_KEY]
-    },
-    mainnet: {
-      url: MAINNET_URL,
-      gasPrice: 50000000000,
-      accounts: [MAINNET_DEPLOY_KEY]
+      accounts: [MILKOMEDA_DEPLOY_KEY]
     }
   },
   etherscan: {
     apiKey: {
-      mainnet: MAINNET_DEPLOY_KEY,
-      arbitrumOne: ARBISCAN_API_KEY,
-      avalanche: SNOWTRACE_API_KEY,
-      bsc: BSCSCAN_API_KEY,
-      polygon: POLYGONSCAN_API_KEY,
+      shimmer: SHIMMER_API_KEY,
+      milkomeda: MILKOMEDA_API_KEY
     }
   },
   solidity: {
